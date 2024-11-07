897f17e9b055980892401b8989c73b92e11d7282
MrArsenic621 <asedighi81@gmail.com>
2024-11-07 20:20:10 +0330

    fix showing eerror mesages in api responses

--- src/api/utils.py
+++ src/api/utils.py
@@ -5,12 +5,13 @@ def api_response(success: bool, message: str, payload=None, error=None, status_c
     """
     Utility function to standardize API responses.
     """
+    error_message = error.__str__() if error else None
     response_structure = ApiResponseSchema(
         status='success' if success else 'error',
         data=DataSchema(
             message=message,
             payload=payload if success else None,
-            error=ErrorSchema(details=error) if not success else None
+            error=ErrorSchema(details=error_message) if not success else None
         )
     )
     return Response(response_structure.dict(), status=status_code)
